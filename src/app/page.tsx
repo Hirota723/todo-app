514b199add33b2e582d451cb2a6ab66560ce2f36
ひろ <koki.hiro0723@gmail.com>
2024-05-19 16:15:36 +0900

    ✨ page.tsxのレイアウトを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import AddTask from "./components/AddTask";
+import TodoTask from "./components/TodoTask";
 
 const Home = () => {
   return (
-    <main>
-      <h1>Todo App</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen  bg-gray-200">
+      <h1 className="text-4xl font-bold text-gray-700 -mt-32">Todo App</h1>
+      <div className="w-full max-w-xl mt-5">
+        <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
+          <AddTask />
+          <TodoTask />
+        </div>
+      </div>
     </main>
   );
 };
